4339e55e6186bf82b19b4cc1f73ece45023a7509
Roman Kuba <roman.kuba@gmail.com>
2024-08-04 12:52:10 +0200

    feat: boilerplate commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Snel Test App
+
+This project is set up with **Vite** and **Vue**. It's purely for demo purposes on how to work with SNEL, and highlight various capabilities.
+
+The app will change in line with features added to SNEL.
+
+## Check out SNEL
+
+Head over to [snel.space](https://snel.space) and get started today.
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+    <div class="welcome">
+        <h1>Some Title</h1>
+
+        <p>Here's some facts about Cats.</p>
+    </div>
+
+    <div class="gallery"></div>
+</template>
+
+<style scoped>
+    .container {
+        width: 100%;
+        margin-left: auto;
+        margin-right: auto;
+        max-width: 60rem;
+    }
+</style>
