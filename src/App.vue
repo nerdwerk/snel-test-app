52a535756a1fc0cbd014bc48e893ee1ddd33a1c3
Roman Kuba <roman.kuba@gmail.com>
2024-08-05 13:44:52 +0200

    feat: added gallery

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,12 @@
         <p>Here's some facts about Cats.</p>
     </div>
 
-    <div class="gallery"></div>
+    <div class="gallery">
+        <img src="https://placehold.co/400x400" alt="" />
+        <img src="https://placehold.co/400x400" alt="" />
+        <img src="https://placehold.co/400x400" alt="" />
+        <img src="https://placehold.co/400x400" alt="" />
+    </div>
 </template>
 
 <style scoped>
@@ -16,5 +21,16 @@
         margin-left: auto;
         margin-right: auto;
         max-width: 60rem;
+    }
+
+    .gallery {
+        display: grid;
+        grid-template-columns: repeat(2, minmax(0, 1fr));
+        gap: 1rem;
+    }
+
+    .gallery img {
+        max-width: 20rem;
+        max-height: 20rem;
     }
 </style>
